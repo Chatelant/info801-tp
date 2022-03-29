15206c260e02f1ba76885f54b65e21a9dfc2076e
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-03-29 17:59:49 +0200

    multiprocess base

--- main.py
+++ main.py
@@ -1,2 +1,51 @@
-if __name__ == "__main__":
-	print("Hello world")
+from time import sleep, perf_counter, time
+from multiprocessing import Process, Lock, Pipe
+
+
+class CahierDesCharges:
+    def __init__(self, requirements, cost, time, quantity):
+        self.requirements = requirements
+        self.cost = cost
+        self.time = time
+        self.quantity = quantity
+
+
+def task():
+    print('Starting a task...')
+    sleep(1)
+    print('done')
+
+
+def client(conn):
+    # Création du cahier des charges
+    print("Client : Elaboration du cahier des charges")
+    cdc = CahierDesCharges(["Du pain", "Des sandwich"], 10000, 5, 20)
+    print("Envoie sur le pipe")
+    conn.send(cdc)
+    conn.close()
+
+    # Synchronisations de deux process
+    # lockMaitre.aquire()
+    # try:
+    #     print("Envoie cdc au maitre")
+    # finally:
+    #     lockMaitre.release()
+
+
+def maitreOeuvre(l_Client):
+    print("Maitre d'Oeuvre : ")
+
+
+if __name__ == '__main__':
+    # Locks
+
+    # Pipes
+    client_conn, maitre_conn = Pipe()
+
+    # Threads
+    p = Process(target=client, args=(client_conn,))
+    p.start()
+
+    print(maitre_conn.recv().requirements)
+
+    p.join()
